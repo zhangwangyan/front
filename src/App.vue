1a54bb50f498f7842056968f5629315ec736aec5
desperado <17805272076@163.com>
2022-11-06 11:56:13 +0800

    前端校验码引入

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,31 @@
       <form class="layui-form layui-form-pane" action="">
         <div class="layui-form-item">
           <label class="layui-form-label">用户名</label>
-          <div class="layui-input-block">
-            <input type="text" name="title" required  lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
+          <validation-provider  name='邮箱' rules="required|email" v-slot="{errors}">
+          <div class="layui-input-inline">
+
+              <input type="text" name="title" required
+                     v-model="name"
+                     lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
+
           </div>
+            <div class="error layui-form-mid"> {{errors[0]}}</div>
+          </validation-provider>
         </div>
         <div class="layui-form-item">
           <label class="layui-form-label">密码</label>
           <div class="layui-input-block">
-            <input type="password" name="title" required  lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
+            <input type="password" name="title" required
+                   v-model="password"
+                   lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
           </div>
         </div>
         <div class="layui-form-item">
           <label class="layui-form-label">验证码</label>
           <div class="layui-input-inline">
-            <input type="text" name="title" required  lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
+            <input type="text" name="title" required
+                   v-model="code"
+                   lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
           </div>
           <div class="layui-form-mid  svg" v-html="svg" @click="getCaptcha"></div>
         </div>
@@ -33,11 +44,32 @@
 
 <script>
 import axios from 'axios'
+import { ValidationProvider, extend } from 'vee-validate'
+// import { required, email } from 'vee-validate/dist/rules'
+//
+// extend('required', {
+//   ...required,
+//   message: '请输入{_field_}内容'
+// })
+// extend('email', email)
+
+import * as rules from 'vee-validate/dist/rules'
+import zh from 'vee-validate/dist/locale/zh_CN'
+for (const rule in rules) {
+  extend(rule, {
+    ...rules[rule],
+    message: zh.messages[rule]
+  })
+}
+
 export default {
   name: 'app',
   data () {
     return {
-      svg: ''
+      svg: '',
+      name: '',
+      password: '',
+      code: ''
     }
   },
   mounted () {
@@ -50,6 +82,9 @@ export default {
         this.svg = res.data.msg
       })
     }
+  },
+  components: {
+    ValidationProvider
   }
 }
 </script>
@@ -72,5 +107,9 @@ input{
 .svg{
   position:relative;
   top:-13px;
+}
+
+.error{
+  color: red;
 }
 </style>
